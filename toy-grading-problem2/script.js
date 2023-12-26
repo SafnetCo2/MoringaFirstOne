ebc8fc2aa32180daf4d4dd1d491ecd06a40d30dd
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-26 17:30:50 +0300

    completed assigment

---
+++ toy-grading-problem2/script.js
@@ -0,0 +1,63 @@
+const balloonContainers = document.getElementById("balloonContainer");
+function random(num) {
+    return Math.floor(Math.random() * num);
+}
+function getRandomStyles() {
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    var mt = random(200);
+    var ml = random(50);
+    var dur = random(5) + 5;
+
+    return `
+
+     background-color: rgba(${r},${g},${b},0.7);
+     color: rgba(${r},${g},${b},0.7);
+  box-shadow: inset -7px -3px 10px
+  rgba(${r - 10},${g - 10},${b - 10}, 0.7);
+  margin: ${mt}px 0 0 ${ml}px;
+  animation: float ${dur}s ease-in infinite
+    `;
+
+}
+function createBalloons(num) {
+    for (var i = num; i > 0; i--) {
+        var balloon = document.createElement("div");
+        balloon.className = "balloon";
+        balloon.style.cssText = getRandomStyles();
+        balloonContainers.append(balloon);
+    }
+}
+function createCards() {
+    let card = document.createElement('div');
+    l
+    card.className = ('cards')
+
+    card.style.textAlign = "left";
+    balloonContainers.append(card)
+
+
+}
+function removeBallons() {
+    balloonContainers.style.opacity = 0;
+    setTimeout(() => {
+        balloonContainers.remove()
+
+    }, 500)
+}
+window.addEventListener('load', () => {
+    createBalloons(30)
+});
+window.addEventListener('click', () => {
+    removeBallons();
+});
+
+
+const refreshBtn = document.getElementById("btnRefresh");
+
+function handleClick() {
+    history.go(0);
+}
+
+refreshBtn.addEventListener("click", handleClick);
